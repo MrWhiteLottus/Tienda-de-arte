466937d58a5e92408cb3ee500ecbf08088597ecd
unknown <Cetecom@SJAPCA03LC0202.duoc.acad>
2024-05-08 19:57:59 -0400

    Validar nombre

--- Tienda-de-arte/Home/js/scripts.js
+++ Tienda-de-arte/Home/js/scripts.js
@@ -1,7 +1,35 @@
-/*!
-* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Iniciar seccion 
+function validateForm() {
+        var nombre = document.getElementById("username").value;
+        var repetir_contraseña = document.getElementById("password-repeat").value;
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+
+        // Validar si el campo de correo electrónico está vacío o no es válido
+        if (email.trim() === "" || !validateEmail(email)) {
+            document.getElementById("error").innerHTML = "Por favor, introduce un correo electrónico válido.";
+            return false;
+        }
+
+        // Validar si el campo de contraseña está vacío
+        if (password.trim() === "") {
+            document.getElementById("error").innerHTML = "Por favor, introduce tu contraseña.";
+            return false;
+        }
+
+        if (nombre.lenght() < 3 ){
+            document.getElementById("error").innerHTML = "Por favor, Elige un nombre más largo"
+            return false;
+        }
+
+        // Si ambos campos son válidos, devuelve true para enviar el formulario
+        return true;
+    }
+
+    // Función para validar el formato del correo electrónico
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+// fin de iniciar seccion
+
